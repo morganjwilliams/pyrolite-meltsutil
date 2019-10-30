96ff04eb171277983a017662902cb6d7ffa9ac63
Morgan Williams <morgan.j.williams@hotmail.com>
2019-10-30 14:00:20 +0800

    Update Setup.py
    Update setup.py

--- setup.py
+++ setup.py
@@ -45,14 +45,12 @@ setup(
     keywords=["geochemistry", "compositional data", "visualisation", "petrology"],
     packages=find_packages(exclude=["test*"]),
     install_requires=[
+        "pyrolite @ https://github.com/morganjwilliams/pyrolite/tarball/develop#egg=pyrolite",
         "requests",
         "psutil",
         "xmljson",
         "dicttoxml",
         "tqdm",
-    ],
-    dependency_links=[
-        "https://github.com/morganjwilliams/pyrolite/tarball/develop#egg=pyrolite"
     ],
     extras_require={"dev": dev_require},
     tests_require=tests_require,
